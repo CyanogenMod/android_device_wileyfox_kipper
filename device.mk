cb3bb57a6b6d2834996ad23b7cf317f5b558a106
Ethan Chen <intervigil@gmail.com>
2015-04-20 17:21:15 -0700

    kipper: Add keylayout file for hardware buttons
    Change-Id: Ifd7afb48e714830711db5259bf7bd15523c03f71

--- device.mk
+++ device.mk
@@ -53,6 +53,7 @@ PRODUCT_COPY_FILES += \
 
 # Keylayout
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/keylayout/cyttsp4_btn.kl:system/usr/keylayout/cyttsp4_btn.kl \
     $(LOCAL_PATH)/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl
 
 # Lights
