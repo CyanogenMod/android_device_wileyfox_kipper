12c96f440d17158df2412eac8c7dea8b512ddb30
Ethan Chen <intervigil@gmail.com>
2015-04-23 16:59:18 -0700

    kipper: Add log tag to sensors
    Change-Id: Id023e4db0ffcfb2a61461b8dfd602b60aa559c07

--- sensors/NativeSensorManager.cpp
+++ sensors/NativeSensorManager.cpp
@@ -26,6 +26,8 @@ WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
 OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
 IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 --------------------------------------------------------------------------*/
+#define LOG_TAG "NativeSensors"
+
 #include "NativeSensorManager.h"
 
 ANDROID_SINGLETON_STATIC_INSTANCE(NativeSensorManager);
