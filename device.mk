c2c8f7e82c17f324a9e7d7303a21ade03e794b23
Ricardo Cerqueira <ricardo@cyngn.com>
2015-05-20 16:26:25 +0100

    ril: Add data configuration files for DPM2 modem
    Change-Id: Ibd80f176d8fbbb00cd11955a2f3c21cc4f0c0671

--- device.mk
+++ device.mk
@@ -85,6 +85,16 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     init.target.rc
 
+# RIL
+ifeq ($(QCPATH),)
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/data/netmgr_config.xml:system/etc/data/netmgr_config.xml \
+    $(LOCAL_PATH)/configs/data/qmi_config.xml:system/etc/data/qmi_config.xml \
+    $(LOCAL_PATH)/configs/data/dsi_config.xml:system/etc/data/dsi_config.xml \
+    $(LOCAL_PATH)/configs/data/NsrmConfiguration.xml:system/etc/dpm/nsrm/NsrmConfiguration.xml \
+    $(LOCAL_PATH)/configs/data/dpm.conf:system/etc/dpm/dpm.conf
+endif
+
 # Sensors
 PRODUCT_PACKAGES += \
     libcalmodule_common \
