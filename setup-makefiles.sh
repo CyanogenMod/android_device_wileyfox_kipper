78530d194249260672eb981b865c7d7d764d4597
Ethan Chen <intervigil@gmail.com>
2015-09-02 13:55:42 -0700

    kipper: Add wappush jar
    Change-Id: Ief037330472d1045cb6c9f5b98c2d8d1353f75a2

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -106,6 +106,9 @@ PRODUCT_PACKAGES += \\
     TimeService
 
 PRODUCT_PACKAGES += \\
+    com.qrd.wappush
+
+PRODUCT_PACKAGES += \\
     qcnvitems \\
     qcrilhook
 
@@ -222,6 +225,16 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_SUFFIX := \$(COMMON_ANDROID_PACKAGE_SUFFIX)
 LOCAL_MODULE_CLASS := APPS
 LOCAL_CERTIFICATE := platform
+include \$(BUILD_PREBUILT)
+
+include \$(CLEAR_VARS)
+LOCAL_MODULE := com.qrd.wappush
+LOCAL_MODULE_OWNER := $VENDOR
+LOCAL_SRC_FILES := proprietary/framework/com.qrd.wappush.jar
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_SUFFIX := \$(COMMON_JAVA_PACKAGE_SUFFIX)
+LOCAL_MODULE_CLASS := JAVA_LIBRARIES
+LOCAL_CERTIFICATE := PRESIGNED
 include \$(BUILD_PREBUILT)
 
 include \$(CLEAR_VARS)
