36fbb7dc8527e6b4337bb1441a9488e17b9a2049
Ethan Chen <intervigil@gmail.com>
2016-03-02 11:10:21 -0800

    kipper: Remove QC location stack
    * Don't use QC location APK, there are still issues with the fused
      provider.
    
    REF: CYNGNOS-1514
    Change-Id: Ic508349d1bc02a03b489f4bac1097f964b1d66a7

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -98,9 +98,6 @@ PRODUCT_PACKAGES += \\
     cktnvtool
 
 PRODUCT_PACKAGES += \\
-    com.qualcomm.location
-
-PRODUCT_PACKAGES += \\
     qcrilmsgtunnel \\
     shutdownlistener \\
     TimeService
@@ -182,17 +179,6 @@ LOCAL_MODULE_OWNER := $VENDOR
 LOCAL_SRC_FILES := proprietary/bin/cktnvtool
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS  := EXECUTABLES
-include \$(BUILD_PREBUILT)
-
-include \$(CLEAR_VARS)
-LOCAL_MODULE := com.qualcomm.location
-LOCAL_MODULE_OWNER := $VENDOR
-LOCAL_SRC_FILES := proprietary/priv-app/com.qualcomm.location/com.qualcomm.location.apk
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_SUFFIX := \$(COMMON_ANDROID_PACKAGE_SUFFIX)
-LOCAL_MODULE_CLASS := APPS
-LOCAL_PRIVILEGED_MODULE := true
-LOCAL_CERTIFICATE := platform
 include \$(BUILD_PREBUILT)
 
 include \$(CLEAR_VARS)
